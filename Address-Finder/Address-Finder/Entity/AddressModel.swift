38b676bca0caf2f582d22e0aa5f6bfd81f4f96e4
dy-yb <ydyelin@gmail.com>
2021-08-30 21:23:50 +0900

    Add Address detail View with Naver map

---
+++ Address-Finder/Address-Finder/Entity/AddressModel.swift
@@ -0,0 +1,13 @@
+//
+//  AddressModel.swift
+//  Address-Finder
+//
+//  Created by Daye on 2021/08/29.
+//
+
+import Foundation
+
+struct SelectedLocation {
+    var latitude: Double
+    var longitude: Double
+}
---
+++ Address-Finder/Address-Finder/Views/AddressDetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  AddressDetailViewController.swift
+//  Address-Finder
+//
+//  Created by Daye on 2021/08/28.
+//
+
+import UIKit
+import NMapsMap
+
+class AddressDetailViewController: UIViewController {
+
+    let presenter = SearchAddressPresenter()
+    let mapView = NMFMapView(frame: CGRect(x: 0, y: 0, width: 500, height: 500))
+    
+    var selectedLocation = SelectedLocation(latitude: 37.345455350524844, longitude: 126.68751058508818)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setMarker()
+        setCamera()
+        
+        view.addSubview(mapView)
+    }
+    
+    func setCamera(){
+        let camPosition = NMGLatLng(lat: selectedLocation.latitude, lng: selectedLocation.longitude)
+        let cameraUpdate = NMFCameraUpdate(scrollTo: camPosition)
+        mapView.moveCamera(cameraUpdate)
+    }
+
+    func setMarker(){
+        let marker = NMFMarker()
+        
+        marker.position = NMGLatLng(lat: selectedLocation.latitude, lng: selectedLocation.longitude)
+        marker.iconImage = NMF_MARKER_IMAGE_BLACK
+        marker.iconTintColor = UIColor.red
+        marker.width = 25
+        marker.height = 30
+        marker.mapView = mapView
+    }
+    
+}
+    
